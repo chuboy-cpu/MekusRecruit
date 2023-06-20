61691ae7c48c19f5b076530e9a20028b00841bec
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-06-20 16:52:12 +0100

    complete functional modal

--- js/mekus.js
+++ js/mekus.js
@@ -1,3 +1,10 @@
 function modal (e){
-    
+    modal_pic = document.querySelector('.modal-pic');
+    modal_pic.src = document.querySelector(`#img${e.id}`).src;
+
+    modal_role = document.querySelector('.modal-role');
+    modal_role.innerHTML = document.querySelector(`#writeup${e.id} h3`).innerHTML;
+
+    modal_details = document.querySelector('.modal-details');
+    modal_details.innerHTML = document.querySelector(`#writeup${e.id} p`).innerHTML;
 }
