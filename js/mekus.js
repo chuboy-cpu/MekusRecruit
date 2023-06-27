7bbd1b6a4a9339400386ec096a430df965bf7e1a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-06-27 11:34:59 +0100

    still working on the message page

--- js/mekus.js
+++ js/mekus.js
@@ -74,3 +74,9 @@ function signup(){
 
     alert(obj[email_email])
 }
+
+
+function screen_height(){
+    let height = window.innerHeight;
+    document.getElementById("contact-box").style.maxHeight = height + "px";
+}
